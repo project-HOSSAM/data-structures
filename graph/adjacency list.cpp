c49693d6a60ac304de29c1886d152a69e35b63e3
Project Hossam <abouiba099@gmail.com>
2021-05-04 01:48:30 +0000

    simple adjacency list implementation

--- graph/adjacency list.cpp
+++ graph/adjacency list.cpp
@@ -1,38 +1,100 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <unordered_map>
+#include <utility>
 
+typedef std::pair<std::string, int> Edge;
 
 struct Vertex {
     std::string vertex;
-    int weight;
-    Vertex(std::string v, int w){
-        this->vertex = v;
-        this->weight = w;
-    }
+    std::vector<Edge> edges;
+
+    Vertex(std::string s) :vertex(s) {};
 };
 
 class Graph {
 private:
-    std::unordered_map<std::string,std::vector<Vertex>> graph;
+    std::vector<Vertex> graph;
 public:
+    unsigned long size() const;
     void addVertex(std::string);
     void addEdge(std::string, std::string, int);
-    void remove(std::string, std::string);
+    void removeEdge(std::string, std::string);
 };
 
-void Graph::addVertex(std::string newVertex) {
-    this->graph[newVertex] = std::vector<Vertex>{};
+unsigned long Graph::size() const {
+    auto i = this->graph.size();
+    return this->graph.size();
 };
+
+void Graph::addVertex(std::string vertex) {
+    this->graph.push_back(Vertex(vertex));
+}
 
 void Graph::addEdge(std::string v1, std::string v2, int weight) {
-    for(auto edges : this->graph[v1]) {
-        if(edges.vertex == v2) {
-            return;
+    if(v1 == v2) return;
+
+    int indexV1 = -1;
+    int indexV2 = -1;
+    for(int i = 0; i < this->graph.size(); i++) {
+        if(v1 == this->graph[i].vertex) {
+            indexV1 = i;
+        } else if (v2 == this->graph[i].vertex) {
+            indexV2 = i;
         }
     };
-    this->graph[v1].push_back(Vertex(v2,weight));
-    this->graph[v2].push_back(Vertex(v1,weight));
+    if(indexV1 < 0 || indexV2 < 0) return;
 
-};
+    // conect 
+    bool alreadyConnected = false;
+    for(auto edge : this->graph[indexV1].edges) {
+        if(edge.first == v2) {
+            alreadyConnected = true;
+        } 
+    };
+    if(alreadyConnected) return;
+
+    Edge newEdge = std::make_pair(v2, weight);
+    this->graph[indexV1].edges.emplace_back(newEdge);
+
+    newEdge = std::make_pair(v1, weight);
+    this->graph[indexV2].edges.emplace_back(newEdge);
+}
+
+void Graph::removeEdge(std::string v1, std::string v2) {
+    if(v1 == v2) return;
+
+    int indexOfV1Edge = -1;
+    int indexOfV2Edge = -1;
+    int indexV1 = -1;
+    int indexV2 = -1;
+    for(int i = 0; i < this->graph.size(); i++) {
+        if(v1 == this->graph[i].vertex) {
+            indexV1 = i;
+        } else if (v2 == this->graph[i].vertex) {
+            indexV2 = i;
+        }
+    };
+    if(indexV1 < 0 || indexV2 < 0) return;
+
+    // finding v1 edge index
+    for(int i = 0; i < this->graph[indexV1].edges.size(); i++) {
+        if(this->graph[indexV1].edges[i].first == v2) {
+            indexOfV1Edge = i;
+        }
+    }
+
+    // finding v2 edge index
+    for(int i = 0; i < this->graph[indexV2].edges.size(); i++) {
+        if(this->graph[indexV2].edges[i].first == v1) {
+            indexOfV2Edge = i;
+        }
+    }
+
+    if(indexOfV1Edge < 0 || indexOfV2Edge < 0) return;
+
+    // removing
+    this->graph[indexV1].edges.erase( this->graph[indexV1].edges.begin() + indexOfV1Edge);
+    this->graph[indexV2].edges.erase( this->graph[indexV2].edges.begin() + indexOfV2Edge);
+}
+
